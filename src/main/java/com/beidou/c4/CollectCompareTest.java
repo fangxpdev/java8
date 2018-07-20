82fd5d651a09b9df430cd6d732b213d75746e649
fangxp <fangxpdev@gmail.com>
2018-07-20 10:44:44 +0800

    java8 c 4

--- src/main/java/com/beidou/c4/CollectCompareTest.java
+++ src/main/java/com/beidou/c4/CollectCompareTest.java
@@ -8,6 +8,10 @@ import static java.util.Comparator.*;
 import static java.util.stream.Collectors.*;
 
 /**
+ *
+ * 中间操作 返回流
+ * 终端操作 返回结果  list 具体值
+ *
  * @author beidou
  * @since 18/7/19
  */
@@ -41,7 +45,33 @@ public class CollectCompareTest {
 
         System.out.println(lowDishName);
 
+        List<String> highDishName = menu.stream()
+                .filter(d -> {
+                    System.out.println("filtering" + d.getName());
+                    return d.getCalories() > 300;})
+                .map(Dish::getName)
+                .limit(3)
+                .collect(toList());
+        System.out.println(highDishName);
 
+        /*
+         * filter map 两个独立操作，但它们合并到同一个遍历中（循环遍历）
+         * limit 出发短路  所以只打印了3条对应记录
+         */
+        List<String> highDishName2 = menu.stream()
+                .filter(d -> {
+                    System.out.println("filtering " + d.getName());
+                    return d.getCalories() > 300;})
+                .map(d->{
+                    System.out.println("mapping " + d.getName());
+                    return d.getName();})
+                .limit(3)
+                .collect(toList());
+        System.out.println(highDishName2);
+
+        menu.stream().forEach((Dish d) -> System.out.println(d.getName()));
+
+        menu.stream().forEach(System.out::println);
 
 
     }
